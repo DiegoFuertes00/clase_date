08be30171e186a77b1350c3d5a68718bc4eab5e6
Diego Fuertes López <dfuerl00@estudiantes.unileon.es>
2022-11-08 01:51:20 +0100

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,45 @@
+class date(){
+    int day;
+    int month;
+    int year;
+    boolean isSameYear(Date another){
+        return this.year==another.Getyear();
+    }
+//ahora con if
+    boolean isSameYear(Date another){
+        if (this.year==another.getYear()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    string getMonthName(){
+        switch(this.month){
+            case 1:
+                monthName="Enero";
+                break;
+            case 2:
+            //todos los casos
+            return monthName;
+        }
+    }
+    private int getNumDaysMonth(int month){}
+    int getNumDaysSinceStart(){
+    int i, numDays=this.day;
+    for(i=this.month -1,i>=1,i--){
+        numDays=getNumDaysMonth(i);
+    }
+    return numDays;    
+    }
+    Date getRandomDate();
+    int getNumAttemptsToDay(){
+        int numTries=0;
+        Date date;
+        do{
+            date=getRandomDate();
+            numTries++;
+
+        }while(!date.equals(this));//!this.equals(date)
+        return numTries;
+    }
+}
---
+++ MainDate.java
@@ -0,0 +1,9 @@
+class MainDate{
+    public static void main (String args[]){
+        Date hoy=new Date(26,10,2022);
+        Date ayer=new Date(25,10,2022);
+        S.o.p.(hoy.isSameYear(ayer));
+        hoy.getNumDaysSinceStart();
+        
+    }
+}
